c5f3ecf6b96f762c06deb4e8f5005f9e30461ce2
Deepthi Dasappa <deepthidas19@gmail.com>
2024-11-21 12:16:46 +0530

    Fix for the public images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="flex flex-col items-center mt-8 mb-16">
       <img
-        src="src\assets\logo.png"
+        src="logo.png"
         alt="an-art-logo"
         className="-mt-4 mb-3 w-28 h-28 object-contain"
       />
